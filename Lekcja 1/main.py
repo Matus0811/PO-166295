54532779aae2867b979a6657696e2b71225dfb29
Matus0811 <kamil.matusiak2001@wp.pl>
2022-10-09 16:02:28 +0200

    Commit

---
+++ Lekcja 1/main.py
@@ -0,0 +1,24 @@
+#zadanie 1
+lista1a = [x for x in range(1,11)]
+print(lista1a)
+
+lista1b = [x*2 for x in range(0,11)]
+print(lista1b)
+
+lista1c = [x**2 for x in range(1,11)]
+print(lista1c)
+
+lista1d = [x*0 for x in range(1,10)]
+print(lista1d)
+
+lista1e = [x%2 for x in range(1,10)]
+print(lista1e)
+
+lista1f = [x%5 for x in range(1,10)]
+print(lista1f)
+
+#zadanie 2
+x = 0
+while x <11:
+
+
